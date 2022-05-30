fae5c1149948f7ccb47475670b8b8eb7a1bfa9d1
Ahmad Othman <aothman156@gmail.com>
2022-05-31 02:09:10 +0300

    added php file for restaurants page

--- get_restaurants.php
+++ get_restaurants.php
@@ -2,18 +2,13 @@
 header('Access-Control-Allow-Origin: *');  
 include("connection.php");
 
-$r_id=$_POST["idRestaurants"];
-$query = $mysqli->prepare("SELECT * from restaurants where idRestaurants = ? ");
-$query->bind_param("s",$r_id);
+$query = $mysqli->prepare("SELECT * from restaurants");
 $query->execute();
 $array = $query->get_result();
 $response = [];
 while($user = $array->fetch_assoc()){
     $response[] = $user;
 }
-
-
 $json = json_encode($response);
 echo $json;
-
 ?>
