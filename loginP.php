adbfdb705fc49cf5c14b693041fab68d7c1837b3
Ali-olliek <you@example.com>
2022-05-31 23:00:46 +0300

    trying to fix login

--- loginP.php
+++ loginP.php
@@ -1,14 +1,18 @@
 <?php
 include("connection.php");
-$username = $_POST["username"]; //we should get the data from the user, no?
-$password = hash("sha256", $_POST["password"]);
-$query = $mysqli->prepare("Select id from users where username = ? AND password = ?");
+
+$username = $_POST["user_fname"];
+$password = hash("sha256", $_POST["User_password"]);
+echo($password);
+echo($username);
+$query = $mysqli->prepare("Select idUsers from Users where user_fname = ? AND User_password = ?");
 $query->bind_param("ss", $username, $password);
 $query->execute();
 $query->store_result();
 $num_rows = $query->num_rows;
 $query->bind_result($id);
 $query->fetch();
+
 $response = [];
 if($num_rows == 0){
     $response["response"] = "User Not Found";
@@ -16,6 +20,7 @@ if($num_rows == 0){
     $response["response"] = "Logged in";
     $response["user_id"] = $id;
 }
+$json = Null;
 $json == json_encode($response);
 echo $json;
 ?>
